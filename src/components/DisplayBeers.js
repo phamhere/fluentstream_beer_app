8c61369e10ec741fc951aab52ff3d4eed0096a6c
Peter Pham <phamhere@gmail.com>
2019-04-10 23:02:40 -0600

    created basic layout and removed excess files

---
+++ src/components/DisplayBeers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DisplayBeers extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>DisplayBeers Page!</h1>
+      </div>
+    );
+  }
+}
+
+export default DisplayBeers;
